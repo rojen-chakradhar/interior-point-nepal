d9d058bc9acf1476aab187a515233f9a1d6725f4
Rojen Chakradhar <chakradhar@rojen.me>
2026-01-20 23:44:41 +0545

    menu work completely now

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
 function openMenu() {
   let menu = document.querySelector("aside");
   menu.style.display = "flex";
-  console.log("works");
 }
 
 function closeMenu() {
   let menu = document.querySelector("aside");
   menu.style.display = "none";
-  console.log("works");
 }
